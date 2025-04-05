b1c2e10c9d87e5efa97357e5026f1ce93fb7ee43
younes-ouhra <g.younes36@gmail.com>
2025-04-05 22:01:26 +0200

    Update jk_flip_flop_source_code.vhd

--- Design Sources/jk_flip_flop_source_code.vhd
+++ Design Sources/jk_flip_flop_source_code.vhd
@@ -10,15 +10,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 
-entity Aufgabe2_JK is
+entity JK_flip_flop is
 
     Port ( J : in STD_LOGIC;
            K : in STD_LOGIC;
            CLK : in STD_LOGIC;
            Q : out STD_LOGIC);
-end Aufgabe2_JK;
+end JK_flip_flop;
 
-architecture Behavioral of Aufgabe2_JK is
+architecture Behavioral of JK_flip_flop is
 --
 
 --
